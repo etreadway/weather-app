97050f7c83a8e01343eb207cc90bb05c8d78d007
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-10 11:49:46 -0600

    added css and bootstrap

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ var API_KEY = config.API_KEY;
 var zipCode = ''
 var inputObj = document.getElementById('input')
 var buttonObj = document.getElementById('button')
+var dangerObj = document.getElementById('danger')
+var dangerObjClose = document.getElementById('close')
 
+
+// search button functionality
 buttonObj.addEventListener('click', function() {
     zipCode = inputObj.value;
     fetch('http://api.openweathermap.org/data/2.5/weather?zip='+zipCode+'&units=imperial&appid='+API_KEY)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        document.getElementById('zip-code').innerHTML = zipCode
+        document.getElementById('weather').innerHTML = data.weather['0'].description
         document.getElementById('temperature').innerHTML = Math.round(data.main.temp)
         document.getElementById("city").innerHTML = data.name
     })
+    //
+    .catch(error => dangerObj.style.display = 'block')
+})
+
+// alert danger close button
+dangerObjClose.addEventListener('click', function(){
+    dangerObj.style.display = 'none'
 })
 
 
