6a8f2be4304cbb0fcf0a45b8ba5ccf0e1c596661
Eric Treadway <eric.s.treadway@gmail.com>
2021-03-11 09:09:41 -0600

    added search by city

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var API_KEY = config.API_KEY;
-var zipCode = ''
+var userLocation = ''
 var inputObj = document.getElementById('input')
 var buttonObj = document.getElementById('button')
 var dangerObj = document.getElementById('danger')
@@ -9,18 +9,22 @@ var dangerObjClose = document.getElementById('close')
 
 // search button functionality
 buttonObj.addEventListener('click', function() {
-    zipCode = document.getElementById('input').value;
-    fetch('http://api.openweathermap.org/data/2.5/weather?zip='+zipCode+'&units=imperial&appid='+API_KEY)
+    userLocation = document.getElementById('input').value;
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip='+userLocation+'&units=imperial&appid='+API_KEY)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        document.getElementById('weather').innerHTML = data.weather['0'].description
-        document.getElementById('temperature').innerHTML = Math.round(data.main.temp) + '&#8457'
-        document.getElementById('temperature').innerHTML = Math.round(data.main.feels_like) + '&#8457'
-        document.getElementById('city').innerHTML = data.name
-        document.getElementById('weather-icon').src = 'http://openweathermap.org/img/wn/'+ data.weather[0].icon +'@2x.png'
+        placeInfoOnPage(data)
+        
     })
-    .catch(error => dangerObj.style.display = 'block')
+    .catch(error => fetch('http://api.openweathermap.org/data/2.5/weather?q='+userLocation+'&units=imperial&appid='+API_KEY)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                placeInfoOnPage(data)
+            })
+            .catch(error => dangerObj.style.display = 'block')
+        )
 })
 
 // alert danger close button
@@ -28,4 +32,11 @@ dangerObjClose.addEventListener('click', function(){
     dangerObj.style.display = 'none'
 })
 
+function placeInfoOnPage(data) {
+    document.getElementById('weather').innerHTML = data.weather['0'].description
+    document.getElementById('temperature').innerHTML = Math.round(data.main.temp) + '&#8457'
+    document.getElementById('temperature').innerHTML = Math.round(data.main.feels_like) + '&#8457'
+    document.getElementById('city').innerHTML = data.name
+    document.getElementById('weather-icon').src = 'http://openweathermap.org/img/wn/'+ data.weather[0].icon +'@2x.png'
 
+}
