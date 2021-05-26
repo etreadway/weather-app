3185d7cd8533275e932280a174459a53240a6951
Eric Treadway <eric.s.treadway@gmail.com>
2021-05-26 16:47:29 -0500

    reverting changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var API_KEY = process.env.API_KEY;
+var API_KEY = config.API_KEY
 var userLocation = ''
 var inputObj = document.getElementById('input')
 var buttonObj = document.getElementById('button')
